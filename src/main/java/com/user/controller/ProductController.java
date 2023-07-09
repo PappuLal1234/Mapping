7d973824b5c13497550070e6fa4d8639673fa38d
Pappu <partushme12345@gmail.com>
2023-07-09 20:43:59 +0530

    First commit

---
+++ src/main/java/com/user/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.user.controller;
+import com.user.entities.Product;
+import com.user.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @GetMapping("/{productId}")
+    public Product getProductById(@PathVariable Integer productId) {
+        return productService.getProductById(productId);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping(params = "category")
+    public List<Product> getProductsByCategory(@RequestParam String category) {
+        return productService.getProductsByCategory(category);
+    }
+
+    @DeleteMapping("/{productId}")
+    public void deleteProductById(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+    }
+}
